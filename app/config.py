b03277a94980d949155fd3d1ae25e8a8c3658da0
Moses Karani <karanim594@gmail.com>
2022-05-08 16:43:20 +0300

    added config classes

---
+++ app/config.py
@@ -0,0 +1,21 @@
+import os 
+
+class Config:
+
+    pass
+
+class DevConfig(Config):
+
+
+    DEBUG = True
+
+class ProdConfig(Config):
+
+
+    pass 
+
+
+config_options = {
+    'development':DevConfig,
+    'production' :ProdConfig
+}
