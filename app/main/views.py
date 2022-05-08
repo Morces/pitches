711e78d427e63c457bd235f0583c4e4a1b2a113b
Moses Karani <karanim594@gmail.com>
2022-05-08 17:44:38 +0300

    created a funtion that renders pitches.html

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,4 @@
+from app.main.forms import PitchForm
 from . import main
 from flask import render_template
 
@@ -5,3 +6,11 @@ from flask import render_template
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/pitches', methods=['GET', 'POST'])
+def pitches():
+
+    pitch_form = PitchForm()
+
+    return render_template('pitches.html', pitch_form=pitch_form)
+    
