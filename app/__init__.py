64bf737c757bcb254155fe66f0df2e994df0d9ab
Moses Karani <karanim594@gmail.com>
2022-05-08 16:25:55 +0300

    instantiated the flask app

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,17 @@
+import os
 
+from flask import Flask
+from config import config_options
+
+
+
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Initializing configurations
+    app.config.from_object(config_options[config_name])
+
+    # Registering Blueprints
 
