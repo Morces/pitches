9c846290f6350f9c21167d9b1f26d61d5c74aba2
Moses Karani <karanim594@gmail.com>
2022-05-09 11:35:17 +0300

    modified views

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,9 @@
+from flask_login import current_user, login_required
 from app.main.forms import PitchForm
+from app.models import Dislikes, Likes, Pitch, User
 from . import main
-from flask import render_template
+from flask import redirect, render_template, url_for
+from .. import db
 
 
 @main.route('/')
@@ -12,13 +15,54 @@ def pitches():
 
     pitch_form = PitchForm()
 
-    
-
-    return render_template('pitches.html', pitch_form=pitch_form)
+    if pitch_form.validate_on_submit():
+        pitch = Pitch(pitch=pitch_form.pitch.data, name=pitch_form.name.data)
+        db.session.add(pitch)
+        db.session.commit()
+        return redirect(url_for('.pitches'))
+    posts = Pitch.query.all()
+    return render_template('pitches.html',posts=posts,pitches=pitches, pitch_form=pitch_form)
 
 @main.route('/user/<username>')
 def profile(username):
+    user = User.query.filter_by(username=username).first()
+    posts = user.pitches.all()
+    
+    return render_template('profile.html', user=user, pitches=posts)
+
+@main.route('/like/<init:id>', methods=['GET', 'POST'])
+@login_required
+def like(id):
+    get_pitches = Likes.get_likes(id)
+    valid_string = f'{current_user.id}:{id}'
+    for pitch in get_pitches:
+        to_str = f'{pitch}'
+        print(valid_string + " " + to_str)
+        if valid_string == to_str:
+            return redirect(url_for('main.pitches', id=id))
+        else:
+            continue
+    new_vote = Likes(user=current_user, pitch_id=id)
+    new_vote.save()
+    return redirect(url_for('main.pitches', id=id))
 
 
-    return render_template('profile.html')
+@main.route('/like/<init:id>', methods=['GET', 'POST'])
+@login_required
+def dislike(id):
+    get_pitches = Dislikes.get_dislikes(id)
+    valid_string = f'{current_user.id}:{id}'
+    for pitch in get_pitches:
+        to_str = f'{pitch}'
+        print(valid_string + " " + to_str)
+        if valid_string == to_str:
+            return redirect(url_for('main.pitches', id=id))
+        else:
+            continue
+    new_vote = Dislikes(dislike=current_user, pitch_id=id)
+    new_vote.save()
+    return redirect(url_for('main.pitches', id=id))
+
+
+
     
