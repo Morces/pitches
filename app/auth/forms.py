616b77308f1afcc1304388c32abbdd11603e4462
Moses Karani <karanim594@gmail.com>
2022-05-09 12:24:17 +0300

    modified navbar and optimized register and login templates

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,3 @@
-from ast import Sub
-import email
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,ValidationError,BooleanField
 from wtforms.validators import InputRequired,Email,EqualTo
