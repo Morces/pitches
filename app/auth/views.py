06b1c9e925838550502c835fbc59e7db6d397f11
Moses Karani <karanim594@gmail.com>
2022-05-09 12:03:37 +0300

    defined login view

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,14 +1,25 @@
-from flask import redirect, render_template, url_for
-
-from app.auth.forms import RegistratioForm
+from app.auth.forms import LoginForm, RegistratioForm
 from app.models import User
-from . import auth
+from flask import flash, redirect, render_template, request, url_for
+from flask_login import login_user
+
 from .. import db
+from . import auth
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email=login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+        title = "Login"
+    return render_template('auth/login.html', login_form=login_form, title=title)
     
 
 @auth.route('/register', methods=['GET','POST'])
