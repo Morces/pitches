d5e2d27ac93a6844b7145c77ba5a98310888914f
Moses Karani <karanim594@gmail.com>
2022-05-10 13:53:45 +0300

    deployment

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ from app import create_app, db
 from app.models import  User,Pitch
 
 #Creating app instance
-app = create_app('development')
+app = create_app('production')
 manager = Manager(app)
 migrate = Migrate(app,db)
 
